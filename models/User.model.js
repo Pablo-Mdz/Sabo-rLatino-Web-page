66d3a6da120af47ffe80e4794e0ac7fcdb78f50c
Pablo Cigoy <pablocigoy@gmail.com>
2022-11-21 12:27:13 +0100

    first folders login working

---
+++ models/User.model.js
@@ -0,0 +1,32 @@
+
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: false,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    img: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user"
+    }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`    
+    timestamps: true
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
