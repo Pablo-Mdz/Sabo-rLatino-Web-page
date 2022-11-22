39d4ea2e518ac369b7ffcec49aff61cc72f21a20
Pablo Cigoy <pablocigoy@gmail.com>
2022-11-22 15:24:57 +0100

    create, read and edit working

--- routes/restaurants.route.js
+++ routes/restaurants.route.js
@@ -6,13 +6,23 @@ router.get('/restaurants/create', (req, res) => {
     res.render('restaurants/new')
 });
 
+
+// read restaurant
+// router.get('/restaurants/read', (req, res) => {
+//     Restaurant.find()
+//     .then(restaurant => res.render('restaurants', { restaurant }))
+//     .catch(err => console.log(err))
+//     res.render('restaurants/details')
+// });
+
+
 router.post('/restaurants/create', (req, res) => {
-    const {name, description, speciality, tel, url, email, street, houseNumber, area, /* owner */ } = req.body
+    const { name, description, speciality, tel, url, email, street, houseNumber, area /* owner */ } = req.body
     console.log(req.body)
     Restaurant.create({
         name,
         description,
-        speciality, 
+        speciality,
         tel,
         url,
         email,
@@ -25,54 +35,56 @@ router.post('/restaurants/create', (req, res) => {
 });
 
 //get restaurant
-router.get('/restaurants', (req, res) => {
-	Restaurant.find()
-		.then(restaurant => res.render('restaurants/restaurants', { restaurant }))
-		.catch(err => console.log(err))
+router.get('/restaurants/rest', (req, res) => {
+    Restaurant.find()
+        .then(restaurant => res.render('restaurants/rest', { restaurant }))
+        .catch(err => console.log(err))
 });
 
 //get details
 router.get("/restaurants/:id", (req, res) => {
-    const id= req.params.id
+    const id = req.params.id
 
     Restaurant.findById(id)
-    .then(restaurant => res.render("restaurants/details", {restaurant}))
-    .catch(err=> console.log(err))
+        .then(restaurant => res.render("restaurants/details", { restaurant }))
+        .catch(err => console.log(err))
 })
 
 //delete restaurant
 router.get('/restaurants/:id/delete', (req, res) => {
-	const id = req.params.id
-	/* const query = { _id: id }
-	if (req.user.role !== 'admin') {
-		query.owner = req.user._id
-	} */
-	Restaurant.findByIdAndRemove(id)
-		.then(deletedRestaurant => res.redirect('/restaurants'))
-		.catch(err => console.log(err))
+    const id = req.params.id
+    /* const query = { _id: id }
+    if (req.user.role !== 'admin') {
+        query.owner = req.user._id
+    } */
+    Restaurant.findByIdAndRemove(id)
+        .then(deletedRestaurant => res.redirect('/restaurants'))
+        .catch(err => console.log(err))
 });
 
 //edit service
-router.get("/restaurants/:id/edit", (req, res) => {
+router.get("/restaurants/:id/edit", async (req, res) => {
     const id = req.params.id
-
-    Restaurant.findById(id)
-    .then(restaurant => {
-        res.render("restaurants/edit", { restaurant })
-    })
-    .catch(err => {
+    // Restaurant.findById(id)
+    try {
+        // const rest = await Restaurant.findById(id).populate("restaurants")
+        const restaurant = await Restaurant.findById(id)
+        console.log(restaurant)
+        res.render("restaurants/edit", restaurant)
+    } catch (err) {
         console.log(err)
-    })
+    }
+
 })
 
-router.post("/restaurants/:id/edit", (req, res) => {
+router.post("/restaurants/:id", (req, res) => {
     const id = req.params.id
-    const { name, description, speciality, tel, url, email, street, houseNumber, area, /* owner */ } = req.body
+    const { name, description, speciality, tel, url, email, street, houseNumber, area /* owner */ } = req.body
 
     const restaurant = {
         name,
         description,
-        speciality, 
+        speciality,
         tel,
         url,
         email,
@@ -83,10 +95,12 @@ router.post("/restaurants/:id/edit", (req, res) => {
     }
 
     Restaurant.findByIdAndUpdate(id, restaurant)
-    .then(createdRestaurant => {
-        res.redirect(`/restaurants/${id}`)
-    })
-    .catch(err => console.log(err))
+        .then(createdRestaurant => {
+            res.redirect(`/`)
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
 module.exports = router;
